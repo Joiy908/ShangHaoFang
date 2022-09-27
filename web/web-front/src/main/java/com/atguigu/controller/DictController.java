11a849cfe53f29e48f69ddde8c8a6866a171eb45
joiy908 <joiy9088@gmail.com>
2022-09-27 12:08:37 +0800

    init web-front and impl part1 of render searchCondition

---
+++ web/web-front/src/main/java/com/atguigu/controller/DictController.java
@@ -0,0 +1,40 @@
+package com.atguigu.controller;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.base.controller.BaseController;
+import com.atguigu.entity.Admin;
+import com.atguigu.entity.Dict;
+import com.atguigu.result.Result;
+import com.atguigu.service.AdminService;
+import com.atguigu.service.DictService;
+import com.atguigu.util.AliyunOSSUtil;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value="/dict")
+public class DictController extends BaseController {
+    @Reference
+    private DictService dictService;
+
+    /**
+     * 根据编码获取子节点数据列表
+     */
+    @GetMapping(value = "findListByDictCode/{dictCode}")
+    public Result<List<Dict>> findListByDictCode(@PathVariable String dictCode) {
+        List<Dict> list = dictService.findListByDictCode(dictCode);
+        return Result.ok(list);
+    }
+
+
+}
